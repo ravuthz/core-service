dc9ba439c645e8903ec1064e1ca92fcd5fb316c6
ravuthz <ravuthz@gmail.com>
2021-01-02 01:48:59 +0700

    Prepare configuration for deploy to Heroku

--- src/test/java/com/github/ravuthz/service/ServiceApplicationTests.java
+++ src/test/java/com/github/ravuthz/service/ServiceApplicationTests.java
@@ -3,11 +3,13 @@ package com.github.ravuthz.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(classes = ServiceApplication.class, webEnvironment = RANDOM_PORT)
 class ServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
